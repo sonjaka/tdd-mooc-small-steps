289320ffcd2ae117b7f3440e4f957e1fb6292a81
Sonja Karl <sonja-karl@web.de>
2023-01-03 16:34:39 +0100

    Added plainDate

--- src/prices.mjs
+++ src/prices.mjs
@@ -19,6 +19,7 @@ function createApp(database) {
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
+    const plainDate = parsePlainDate(req.query.date);
     const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
@@ -26,6 +27,12 @@ function createApp(database) {
   function parseDate(dateString) {
     if (dateString) {
       return new Date(dateString);
+    }
+  }
+
+  function parsePlainDate(dateString) {
+    if (dateString) {
+      return Temporal.PlainDate.from(dateString);
     }
   }
 
